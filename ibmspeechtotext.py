018f4a6edf8ed8c77974f2a961d67ac52402668d
Aditi-Mohan <54301942+Aditi-Mohan@users.noreply.github.com>
2021-01-30 22:55:54 +0530

    audio file with path

--- ibmspeechtotext.py
+++ ibmspeechtotext.py
@@ -1,12 +1,14 @@
 from ibm_watson import SpeechToTextV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from write_transcript_to_notion import write_transcript
+# from ../../Desktop import converted.wav
 
 api_key = IAMAuthenticator("acyFMIII31gP3u8wJKIwnDAS6mx5NMiUV698g9VFsodj")
 speech_2_text = SpeechToTextV1(authenticator=api_key)
 speech_2_text.set_service_url("https://api.jp-tok.speech-to-text.watson.cloud.ibm.com/instances/f8ab0ba3-dcfd-427f-8506-d3822ca14d21")
 
-with open("converted.wav", "rb") as audio_file:
+audio_file_path = "../../Desktop/converted.wav"
+with open(audio_file_path, "rb") as audio_file:
     result = speech_2_text.recognize(
         audio=audio_file,
         content_type="audio/wav"
@@ -15,6 +17,7 @@ with open("converted.wav", "rb") as audio_file:
 result = result['results']
 
 text = []
+para_break = 
 for i in result:
     alts = i['alternatives']
     confs = [x['confidence'] for x in alts]
