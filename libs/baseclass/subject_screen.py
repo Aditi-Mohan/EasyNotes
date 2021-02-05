742c58d774892c736c22aecdfe3eae75437906df
Aditi-Mohan <54301942+Aditi-Mohan@users.noreply.github.com>
2021-02-05 10:21:10 +0530

    did things

--- libs/baseclass/subject_screen.py
+++ libs/baseclass/subject_screen.py
@@ -1,8 +1,29 @@
 from kivymd.uix.screen import MDScreen
 from kivy.properties import StringProperty
+from kivymd.uix.list import TwoLineIconListItem, IconLeftWidget
+
+from datetime import datetime
 
 class SubjectScreen(MDScreen):
     title = StringProperty()
+
+    notes = [{'title': 'LESSON 1', 'time': datetime.now().strftime(r"%m/%d/%Y, %H:%M:%S")},
+            {'title': 'LESSON 2', 'time': datetime.now().strftime(r"%m/%d/%Y, %H:%M:%S")},
+            {'title': 'LESSON 3', 'time': datetime.now().strftime(r"%m/%d/%Y, %H:%M:%S")},
+            {'title': 'LESSON 4', 'time': datetime.now().strftime(r"%m/%d/%Y, %H:%M:%S")},
+            {'title': 'LESSON 5', 'time': datetime.now().strftime(r"%m/%d/%Y, %H:%M:%S")},]
     
     def on_pre_enter(self, *args):
         self.title = self.name
+        list_view = self.ids.list_view
+        for i in self.notes:
+            item = TwoLineIconListItem(
+                text= i['title'],
+                secondary_text= i['time']
+            )
+            icon = IconLeftWidget(
+                icon="note-outline",
+                size=(10, 40)
+            )
+            item.add_widget(icon)
+            list_view.add_widget(item)
